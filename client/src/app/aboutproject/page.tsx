5797578387ec3f433515dae75fe5fbd038fa9e10
stha07ashok <stha07ashok@gmail.com>
2025-09-17 12:17:05 +0545

    change

--- client/src/app/aboutproject/page.tsx
+++ client/src/app/aboutproject/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AboutProject = () => {
   return (
     <div className="min-h-[calc(160vh-450px)] px-10 py-22 mx-12 ">
-      AboutProject
+      About-Project
     </div>
   );
 };
